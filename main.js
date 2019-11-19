97477e9fa1fc7189fe0af39a48163cc8c9712288
whuynh11 <45051504+whuynh11@users.noreply.github.com>
2019-11-19 15:25:35 -0800

    Updated media queries for mobile and tablet view, fixed javascript scrolling issue, and added some additional content.

--- main.js
+++ main.js
@@ -9,8 +9,21 @@ $(document).ready(function () {
         $('.top-nav').removeClass('open');
     });
 
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 450);
+    });
+
+    $('#up').on('click', function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 400);
+    });
+
     AOS.init({
         easing: 'ease',
-        duration: 1800
+        duration: 1800,
+        once: true
     });
 });
