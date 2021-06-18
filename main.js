4440fe451b3cd214d6fc132e6926f29a0bc3ae47
whuynh11 <45051504+whuynh11@users.noreply.github.com>
2021-06-18 16:36:59 -0700

    Added title change & updated work experience.

--- main.js
+++ main.js
@@ -26,4 +26,32 @@ $(document).ready(function () {
         duration: 1800,
         once: true
     });
+
+    window.onload = function() {
+
+        var pageTitle = document.title;
+        var attentionMessage = '♪ Baby, Come Back! ♪';
+        var blinkEvent = null;
+      
+        document.addEventListener('visibilitychange', function(e) {
+          var isPageActive = !document.hidden;
+      
+          if(!isPageActive){
+            blink();
+          }else {
+            document.title = pageTitle;
+            clearInterval(blinkEvent);
+          }
+        });
+      
+        function blink(){
+          blinkEvent = setInterval(function() {
+            if(document.title === attentionMessage){
+              document.title = pageTitle;
+            }else {
+              document.title = attentionMessage;
+            }
+          }, 600);
+        }
+      };
 });
